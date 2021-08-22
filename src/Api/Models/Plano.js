e9442d909039a147f55c6b6f663da4202a03b595
danielvdg <daniel.vdg@hotmail.com>
2021-08-22 12:42:49 -0300

    integrando a API

---
+++ src/Api/Models/Plano.js
@@ -0,0 +1,11 @@
+class Plano{
+    constructor(plano,valor,quantidade,promocao,statusDePromocao){
+        this.plano = plano;
+        this.valor = valor;
+        this.quantidade = quantidade;
+        this.promocao = promocao;
+        this.statusDePromocao = statusDePromocao;
+    }
+
+}
+module.exports = Plano
