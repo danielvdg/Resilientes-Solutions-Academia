887a5209696d323562a8498f56406eb8e264eee9
Gustavo Souza <souzagh77@gmail.com>
2021-08-28 15:06:37 -0300

    cadastro post e pagina basica adicionada

---
+++ src/Pages/Cadastro/Cadastro.js
@@ -0,0 +1,82 @@
+import {useEffect, useState} from 'react';
+import * as S from "../Cadastro/style";
+export default function Form(props) {
+
+    var initialValues = {
+        "nome":    '',
+        "senha":   '',
+        "cpf":     '', 
+        "email":   '',
+        "telefone":'',
+        "idade":   '',
+        "cep":     '',     
+        "bairro":  '',   
+        "rua":     '',      
+        "numero":  ''
+    }
+
+    const [values, setValues] = useState(initialValues);
+    //const [posted, setPosted] = useState(false)
+
+    const handleInputChange = (e) => {
+        const { name, value} = e.target;
+        setValues({
+            ...values,
+            [name]: value,
+        });
+    }
+
+    const submit = async (e) => {
+        e.preventDefault();
+    
+        const urlCadastro = 'https://calm-inlet-01792.herokuapp.com/aluno'
+        const options = {
+            method: 'POST',
+            headers: {'Accept': 'application/json','Content-Type': 'application/json'},
+            mode: 'cors',
+            cache: 'default', 
+            body: JSON.stringify(values)
+        }
+        await fetch(urlCadastro, options)
+        //.then(data => setPosted(true))
+        .catch(error => console.log(error))
+
+    }
+
+    const cpf = parseInt(values.cpf)
+    const telefone = parseInt(values.telefone)
+    const idade = parseInt(values.idade)
+    const cep = parseInt(values.cep)
+    const numero = parseInt(values.numero)
+    values.cpf = cpf
+    values.telefone = telefone
+    values.idade = idade
+    values.cep = cep
+    values.numero = numero
+
+    return(
+        <form onSubmit={submit}>
+            <S.Input value={values.nome} onChange={handleInputChange} name="nome" label="nome"/>
+
+            <S.Input value={values.senha} onChange={handleInputChange} name="senha" label="senha"/>
+
+            <S.Input value={values.cpf} onChange={handleInputChange} name="cpf" label="nome" type="number"/>
+            
+            <S.Input value={values.email} onChange={handleInputChange} name="email" label="nome"/>
+
+            <S.Input value={values.telefone} onChange={handleInputChange} name="telefone" label="telefone" type="number"></S.Input>
+            
+            <S.Input value={values.idade} onChange={handleInputChange} name="idade" label="idade" type="number"></S.Input>
+            
+            <S.Input value={values.cep} onChange={handleInputChange} name="cep" label="cep" type="number"></S.Input>
+            
+            <S.Input value={values.bairro} onChange={handleInputChange} name="bairro" label="bairro"></S.Input>
+            
+            <S.Input value={values.rua} onChange={handleInputChange} name="rua" label="rua"></S.Input>
+
+            <S.Input value={values.numero} onChange={handleInputChange} name="numero" label="numero" type="number"></S.Input>
+
+            <button>Cadastrar</button>
+        </form>   
+    )
+}
