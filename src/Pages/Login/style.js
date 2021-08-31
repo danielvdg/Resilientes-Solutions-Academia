e2954f9dab52f639411a3950fa311770d494b307
danielvdg <daniel.vdg@hotmail.com>
2021-08-31 09:45:05 -0300

    ajustes

--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -5,6 +5,9 @@ export const loginContainer = styled.div`
     width: 100%;
     height: 100vh;
     background-color: ${props=> props.theme.cores.cinzaEscuro};  
+    display: flex;
+    align-items: center;
+    justify-content: center;
           
 
    
