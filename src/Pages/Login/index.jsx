55ac419bebf69a5c8f4179e919495c8042d96f05
Gustavo Souza <souzagh77@gmail.com>
2021-08-31 17:49:36 -0300

    login funcional

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,57 +1,90 @@
-import React, {useState} from 'react'
-import * as S from './style'
-import Button from '../../Components/Button'
-
+import {useState, useContext} from 'react';
+import {useHistory, Link} from 'react-router-dom';
+import * as S from './style';
+import {Context} from '../../SessionContext';
 
 const Login = () => {
-    const initialValues = {
-     "email":'',
-     "senha":''
+  const { session, handleLogin } = useContext(Context);
+  const [isHidden, setIsHidden] = useState(true);
+  const history = useHistory();
+  
+  const initialValues = {
+    "email":'',
+    "senha":''
+  }
+
+  const [values, setValues] = useState(initialValues);
+
+  const handleInputChange = (e) => {
+    const { name, value} = e.target;
+    setValues({
+        ...values,
+        [name]: value,
+    });
+  }
+
+  const checkLogin = async (e) => {
+    e.preventDefault();
+
+    const url = 'https://safe-fjord-35975.herokuapp.com/aluno/email/' + values.email
+    const options = {
+      method:'GET'
     }
-
-    const [values, setValues] = useState(initialValues);
-
-    const handleInputChange = (e) => {
-      const { name, value} = e.target;
-      setValues({
-          ...values,
-          [name]: value,
-      });
-    }
-
-    const checkLogin = async (e) => {
-      e.preventDefault();
-
-      const url = 'https://safe-fjord-35975.herokuapp.com/check/'+values.email
-      const options = {
-        method:'GET'
+    await fetch(url,options)
+    .then(response => response.json())
+    .then(data =>
+    {
+      if(data.result.senha === values.senha)
+      {
+        return data.result.id
       }
-      await fetch(url,options)
-      .then((response) => {
-        if(response.status === 500)
-        {
-          console.log(response.error)
-        }
+      else
+        alert('SENHA ERRADA, BABACA!')
+    })
+    .catch(error => alert('Login ou senha incorretos'))
+ 
+  
+    const idAluno = await fetch('https://safe-fjord-35975.herokuapp.com/aluno/email/' + values.email)
+      .then(response => response.json())
+      .then(data => 
+      {
+        handleLogin(data.result.id)
+        return data.result.id
       })
       .catch(error => console.log(error))
-      console.log(url)
-    }
 
-    console.log(values)
+    if(idAluno>0){
+      history.replace('/profile')
+    } 
+    else 
+    {
+      setIsHidden(false)
+      setTimeout(()=>{
+        setIsHidden(true)
+      },2000)
+    } 
+  }
 
-    return (
-        <S.Container>
+  if(session.aluno>0){
+    history.replace('/profile')
+  }
+
+  return (
+    <S.Container>
+
       <S.Form>
         <S.Label htmlFor="label">Nome</S.Label>
         <S.Input id="label" value={values.email} onChange={handleInputChange} name="email"/>              
         <S.Label>Senha</S.Label>
         <S.Input value={values.senha} onChange={handleInputChange} name="senha" />        
-        <button onClick={checkLogin}>Enviar</button>
-      </S.Form>    
-      <S.Link>Equeceu Senha</S.Link>
-      <S.Link>Cadastre-se</S.Link>
+        <S.Button onClick={checkLogin}>Enviar</S.Button>
+      </S.Form> 
+
+      <Link to="/esqueceusenha">Esqueceu Senha</Link>
+      <Link to="/profile">Cadastre-se</Link>
+
     </S.Container>
-    )
+  )
 }
 
 export default Login;
