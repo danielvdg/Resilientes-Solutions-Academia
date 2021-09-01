ab3878e6dce82b5189c3267c828d8c93f72053c4
Gustavo Souza <souzagh77@gmail.com>
2021-09-01 00:10:59 -0300

    aaaaa

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -1,8 +1,21 @@
+import { useContext, useEffect } from 'react';
+import { useHistory, Link, Redirect } from 'react-router-dom';
+import {Context} from '../../SessionContext';
+
+
 export default function Profile(props){
+    const { session, handleLogout } = useContext(Context);
+    const history = useHistory();
+
+    if(session.aluno>0){
 
     return(
         <main>
-            <h1>VOCÊ ESTÁ LOGADO</h1>
+            <h1>TU TA LOGADO IRMAO</h1>
         </main>
     )
+    }
+    else{
+        window.location.href = "http://localhost:3000/";
+    }
 }
