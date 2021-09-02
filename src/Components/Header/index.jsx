2b895e8b2d43ec95c48ea48efc163f2b3ef4bf99
Gustavo Souza <souzagh77@gmail.com>
2021-09-02 03:57:32 -0300

    switch no header, logado/deslogado

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
 import * as S from './style'
 import logo from '../../Assets/logo.png'
+import Cookies from 'universal-cookie'
+
 const Header = () => {
+    const cookies = new Cookies();
+    if(cookies.get('sessaoAluno'))
+        var switchzinho = <S.Link href='/profile'>Minha Conta</S.Link>  
+    else
+        var switchzinho = <S.Link href='/login'>Login</S.Link>
+
     return (
        <S.Container>
            <S.Logo>
@@ -12,7 +19,7 @@ const Header = () => {
            <S.Nav>
                <S.Link href='/nossotime'>Nosso Time</S.Link>
                <S.Link href='/professor'> Professores</S.Link>
-               <S.Link href='/login'>Login</S.Link>
+               {switchzinho}
 
            </S.Nav>
        </S.Container>
